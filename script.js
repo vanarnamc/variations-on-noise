0df0f96b069219ba130c446aa6f44a10ca547276
clinton-van-arnam <113942027+vanarnamc@users.noreply.github.com>
2023-12-08 11:27:19 -0500

    phone

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var words = document.querySelectorAll('.word');
-    var body = document.querySelector('body');
-    var currentWord = 0;
-  
-    function cycleWords() {
+  var words = document.querySelectorAll('.word');
+  var body = document.querySelector('body');
+  var currentWord = 0;
+
+  function cycleWords() {
       // Remove 'visible' class from the current word
       words[currentWord].classList.remove('visible');
-      
+
       // Increment the word index
       currentWord = (currentWord + 1) % words.length;
-      
+
       // Add 'visible' class to the new current word
       words[currentWord].classList.add('visible');
-      
-      // Toggle the inverted class on the body to invert colors
+
+      // Toggle the inverted class on the body to change colors
       body.classList.toggle('inverted');
-    }
-  
-    // Start the cycle with the first word
-    words[currentWord].classList.add('visible');
-    
-    // Set an interval for how often to cycle words (and invert colors)
-    setInterval(cycleWords, 550); // Change word every 2 seconds
-  });
+  }
+
+  // Start the cycle with the first word
+  words[currentWord].classList.add('visible');
+
+  // Set an interval for how often to cycle words (and change colors)
+  setInterval(cycleWords, 550); // Change word every 550 milliseconds
+});
